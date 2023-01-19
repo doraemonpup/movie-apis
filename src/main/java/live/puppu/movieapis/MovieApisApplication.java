cf7df672dc9a3873694a6fb863af34ed669e439c
Chunhawit <pupz28@gmail.com>
2023-01-20 00:53:15 +0100

    create project + do .env things

---
+++ src/main/java/live/puppu/movieapis/MovieApisApplication.java
@@ -0,0 +1,13 @@
+package live.puppu.movieapis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieApisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/live/puppu/movieapis/MovieApisApplicationTests.java
@@ -0,0 +1,13 @@
+package live.puppu.movieapis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieApisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
