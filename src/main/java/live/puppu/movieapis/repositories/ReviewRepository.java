bbd938206b7f2955cabf2265e6d425d89a883a8d
Chunhawit <pupz28@gmail.com>
2023-01-20 20:55:55 +0100

    add Review controller, service and repository

---
+++ src/main/java/live/puppu/movieapis/repositories/ReviewRepository.java
@@ -0,0 +1,10 @@
+package live.puppu.movieapis.repositories;
+
+import live.puppu.movieapis.models.Review;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+}
