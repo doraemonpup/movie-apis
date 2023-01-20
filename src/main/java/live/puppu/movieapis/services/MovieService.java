f1f4f85b3203e684328f628680fbf98345fb963a
Chunhawit <pupz28@gmail.com>
2023-01-20 19:43:32 +0100

    add API to get a single movie

--- src/main/java/live/puppu/movieapis/services/MovieService.java
+++ src/main/java/live/puppu/movieapis/services/MovieService.java
@@ -2,10 +2,12 @@ package live.puppu.movieapis.services;
 
 import live.puppu.movieapis.models.Movie;
 import live.puppu.movieapis.repositories.MovieRepository;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -14,5 +16,9 @@ public class MovieService {
 
     public List<Movie> allMovies() {
         return movieRepository.findAll();
+    }
+
+    public Optional<Movie> singleMovie(String imdbId) {
+        return movieRepository.findMovieByImdbId(imdbId);
     }
 }
