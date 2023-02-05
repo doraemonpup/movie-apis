cd3d88c519edc16979eae7e370775369789c815d
Chunhawit <pupz28@gmail.com>
2023-02-05 17:38:09 +0100

    do the cors stuff

--- src/main/java/live/puppu/movieapis/controllers/MovieController.java
+++ src/main/java/live/puppu/movieapis/controllers/MovieController.java
@@ -5,23 +5,22 @@ import live.puppu.movieapis.services.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
-// Controller -> Service -> Repository (Interface)
+// Controller -> Service -> Repository(Interface)
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/v1/movies")
 public class MovieController {
     @Autowired
     private MovieService movieService;
 
     @GetMapping
+//    @CrossOrigin(origins = "http://localhost:3000")
     public ResponseEntity<List<Movie>> getAllMovies() {
         return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK);
     }
